73c7a31fd81b749ba7e1b89ff73e6301e9773a3a
JoelGeorge693 <joel.darknight18@gmai.com>
2021-07-16 18:04:06 +0530

    increment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [val,incrVal] = useState(0);
+
+  useEffect(()=>{
+    const timeOut = setTimeout(()=>{
+      incrVal(val+1);
+    },1000)
+  },[val]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{val}</p>
     </div>
   );
 }
